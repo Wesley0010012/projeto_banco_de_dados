fd4334cfadb672403e368ac4b9c7071a80d84ed3
Wesley0010012 <wglaurindo33@gmail.com>
2024-05-26 00:21:39 -0300

    refactor: change column names to uppercase

--- development/data/tables.sql
+++ development/data/tables.sql
@@ -3,53 +3,53 @@
 ---------------------------------------
 -- RELACAO CONVENIADOS
 CREATE TABLE IF NOT EXISTS "CONVENIADOS" (
-    cnv_id NUMBER (3),
-    cnv_razao_social VARCHAR2 (30),
-    cnv_cnpj VARCHAR2 (14)
+    CNV_ID NUMBER (3),
+    CNV_RAZAO_SOCIAL VARCHAR2 (30),
+    CNV_CNPJ VARCHAR2 (14)
 );
 -- RELACAO CUSTOS ESTACIONAMENTO
 CREATE TABLE IF NOT EXISTS "CUSTOS_ESTACIONAMENTO" (
-    ces_id NUMBER (5),
-    ces_placa NUMBER (7),
-    ces_data_hora_entrada DATE,
-    ces_data_hora_saida DATE,
-    ces_preco NUMBER (5, 2),
-    ces_tpv_id NUMBER (3)
+    CES_ID NUMBER (5),
+    CES_PLACA NUMBER (7),
+    CES_DATA_HORA_ENTRADA DATE,
+    CES_DATA_HORA_SAIDA DATE,
+    CES_PRECO NUMBER (5, 2),
+    CES_TPV_ID NUMBER (3)
 );
 -- RELACAO MENSALISTAS
 CREATE TABLE IF NOT EXISTS "MENSALISTAS" (
-    mnl_id NUMBER (4),
-    mnl_nome VARCHAR2 (30),
-    mnl_cod_unico VARCHAR2 (32),
-    mnl_data_inclusao DATE,
-    mnl_ativo NUMBER
+    MNL_ID NUMBER (4),
+    MNL_NOME VARCHAR2 (30),
+    MNL_COD_UNICO VARCHAR2 (32),
+    MNL_DATA_INCLUSAO DATE,
+    MNL_ATIVO NUMBER
 );
 -- RELACAO NOTAS FISCAIS
 CREATE TABLE IF NOT EXISTS "NOTAS_FISCAIS" (
-    ntf_id NUMBER (5),
-    ntf_nfe VARCHAR2 (44),
-    ntf_desconto_total NUMBER (5, 2),
-    ntf_ces_id NUMBER (5),
-    ntf_cnv_id NUMBER (3)
+    NTF_ID NUMBER (5),
+    NTF_NFE VARCHAR2 (44),
+    NTF_DESCONTO_TOTAL NUMBER (5, 2),
+    NTF_CES_ID NUMBER (5),
+    NTF_CNV_ID NUMBER (3)
 );
 -- RELACAO PAGAMENTOS
 CREATE TABLE IF NOT EXISTS "PAGAMENTOS" (
-    pgt_id NUMBER(3),
-    pgt_valor NUMBER (5, 2) pgt_mnl_id (4)
+    PGT_ID NUMBER(3),
+    PGT_VALOR NUMBER (5, 2),
+    pgt_MNL_ID (4)
 );
 -- RELACAO TIPOS VAGAS
 CREATE TABLE IF NOT EXISTS "TIPOS_VAGAS" (
-    tpv_id NUMBER (3),
-    tpv_categoria VARCHAR2 (30),
-    tpv_preco NUMBER (3, 2)
+    TPV_ID NUMBER (3),
+    TPV_CATEGORIA VARCHAR2 (30),
+    TPV_PRECO NUMBER (3, 2)
 );
 -- RELACAO VEICULOS
 CREATE TABLE IF NOT EXISTS "VEICULOS" (
-    vcl_id NUMBER (5),
-    vcl_placa VARCHAR2 (7),
-    vcl_tpv_id NUMBER (3),
-    vcl_mnl_id NUMBER (4)
-)
----------------------------------------
+    VCL_ID NUMBER (5),
+    VCL_PLACA VARCHAR2 (7),
+    VCL_TPV_ID NUMBER (3),
+    VCL_MNL_ID NUMBER (4)
+) ---------------------------------------
 -- FIM - RELACOES
 ---------------------------------------
